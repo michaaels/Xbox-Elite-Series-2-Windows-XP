afb7ff633db9cbd64c4a148b180ca9c41ec58861
Michael <58919638+michaaels@users.noreply.github.com>
2026-09-17 00:14:20 -0500

    Add Control Center v1.5 source wrapper

---
+++ control-center/src/XboxElite2ControlCenter.cpp
@@ -0,0 +1,8 @@
+/* XboxElite2ControlCenter v1.5 source wrapper. Files in parts/ are included in original order. */
+#include "parts/XboxElite2ControlCenter_part01.inc"
+#include "parts/XboxElite2ControlCenter_part02.inc"
+#include "parts/XboxElite2ControlCenter_part03.inc"
+#include "parts/XboxElite2ControlCenter_part04.inc"
+#include "parts/XboxElite2ControlCenter_part05.inc"
+#include "parts/XboxElite2ControlCenter_part06.inc"
+#include "parts/XboxElite2ControlCenter_part07.inc"
